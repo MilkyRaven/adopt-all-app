7b581a1de4e2cd18f89563db2702ed41cbefa488
Milky Raven <heartonllamas@gmail.com>
2025-03-15 17:22:09 +0100

    chore: create animals mock data 🐷

--- components/animals/AnimalList.tsx
+++ components/animals/AnimalList.tsx
@@ -1,17 +1,18 @@
 import React from "react";
-import { View, FlatList, Text } from 'react-native';
+import { View, FlatList, TouchableOpacity } from 'react-native';
 import AnimalThumbnail from "./AnimalThumbnail";
+import { animals } from '../../mockData'
 
 export default function AnimalList() {
-
-    const animals = [{ id: '1', name: 'Lucky', species: 'Cat', distance: '10km from you', neutered: 'Neutered', age: 'Junior', image: "https://images.unsplash.com/photo-1511275539165-cc46b1ee89bf" },
-    { id: '2', name: 'Luna', species: 'Dog', distance: '18km from you', neutered: 'Neutered', age: 'Junior', image: "https://images.unsplash.com/photo-1611250282006-4484dd3fba6b" }];
-
     return (
         <View>
+
             <FlatList
                 data={animals}
-                renderItem={({ item }) => <AnimalThumbnail {...item} />}
+                renderItem={({ item }) => (
+                    <TouchableOpacity onPress={() => console.log(item)}>
+                        <AnimalThumbnail {...item} />
+                    </TouchableOpacity>)}
                 keyExtractor={item => item.id}
             />
         </View>
