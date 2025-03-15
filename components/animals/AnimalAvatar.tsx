e8c5ce8445b3274b977663eb03631cc88dd875b8
Milky Raven <heartonllamas@gmail.com>
2025-03-15 17:07:21 +0100

    enhance: change AnimalAvatar props naming

--- components/animals/AnimalAvatar.tsx
+++ components/animals/AnimalAvatar.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import { Image, View } from 'react-native';
 
-export default function AnimalAvatar({ imageUrl, size }: { imageUrl: string, size: 'thumbnail' | 'fullProfile' }) {
+export default function AnimalAvatar({ image, size }: { image: string, size: 'thumbnail' | 'fullProfile' }) {
     const avatarSize = size === 'thumbnail' ? 80 : 150;
 
     return (
         <View style={[{ width: avatarSize, height: avatarSize }]}>
             <Image
-                source={{ uri: imageUrl }}
+                source={{ uri: image }}
                 style={[{ width: avatarSize, height: avatarSize, borderRadius: 8 }]}
                 resizeMode="cover"
             />
