5d42ef14ffdd251a27375ef0b43717b7b3f3be85
Milky Raven <heartonllamas@gmail.com>
2025-03-16 14:14:40 +0100

    refactor: update Theme Provider Wrapper naming

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Slot } from 'expo-router';
-import { ThemeProviderWrapper } from '@/modules/shared/ThemeProvider';
+import { ThemeProviderWrapper } from '@/modules/shared/ThemeProviderWrapper';
 
 export default function App() {
 
