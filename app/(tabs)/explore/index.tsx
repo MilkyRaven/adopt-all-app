6e15262bf68afb1e2ce0b8a2a7acdd25c63cad2c
Milky Raven <heartonllamas@gmail.com>
2025-03-16 17:11:33 +0100

    refactor: add filter modal to Explore layout and update Explore screen

--- app/(tabs)/explore/index.tsx
+++ app/(tabs)/explore/index.tsx
@@ -2,6 +2,8 @@ import { View } from "react-native";
 import LocationTracker from '@/components/location/LocationTracker';
 import AnimalList from "@/components/animals/AnimalList";
 import WelcomeBanner from "@/components/WelcomeBanner";
+import AnimalFilter from "@/components/animals/AnimalFilter";
+import { Link } from "expo-router";
 
 export default function Explore() {
   return (
@@ -13,6 +15,9 @@ export default function Explore() {
     >
       <WelcomeBanner />
       <LocationTracker />
+      <Link href={"/explore/modal"}>
+        <AnimalFilter />
+      </Link>
       <AnimalList />
     </View>
   );
