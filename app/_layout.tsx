1ab99864eb3f671136a09f755ec4bedab193faaf
Milky Raven <heartonllamas@gmail.com>
2025-03-17 12:27:55 +0100

    refactor: update ApplicationList and ApplicationThumbnail imports in routes

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Slot } from 'expo-router';
-import { ThemeProviderWrapper } from '@/modules/shared/ThemeProviderWrapper';
+import { ThemeProviderWrapper } from '@/modules/shared/themes/ThemeProviderWrapper';
 
 export default function App() {
 
