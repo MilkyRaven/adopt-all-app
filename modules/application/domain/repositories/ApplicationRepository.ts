cf267a28bb2985500301822a7e5e351398e93fb5
Milky Raven <heartonllamas@gmail.com>
2025-03-17 13:06:50 +0100

    enhancement: add more use cases to application module (create, update, delete)

--- modules/application/domain/repositories/ApplicationRepository.ts
+++ modules/application/domain/repositories/ApplicationRepository.ts
@@ -2,6 +2,9 @@ import { Application } from "../entities/Application";
 export interface ApplicationRepository {
   findAllApplicationsByUser(userId: string): Promise<Application[]>;
   createApplication(application: Application): Promise<string>;
-  updateApplication(application: Application): Promise<void>;
+  updateApplication(
+    applicationId: string,
+    applicationData: Partial<Application>
+  ): Promise<void>;
   deleteApplication(id: string): Promise<void>;
 }
