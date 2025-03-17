cda0571ff47ad8e695e2855ff9633aa5f95df221
Milky Raven <heartonllamas@gmail.com>
2025-03-17 18:15:56 +0100

    enhancement: allow Application Details to edit application through useApplicationForm hook

--- app/(tabs)/applications/[id].tsx
+++ app/(tabs)/applications/[id].tsx
@@ -1,9 +1,15 @@
-import { View, Text, Button, TextInput, ActivityIndicator } from 'react-native'
+import { View, Text, Button, ActivityIndicator } from 'react-native'
 import { useLocalSearchParams, router } from 'expo-router';
 import useDeleteApplication from '@/modules/application/infraestructure/hooks/useDeleteApplication';
+import useApplicationForm from '@/modules/application/infraestructure/hooks/useApplicationForm';
+import ApplicationForm from '@/modules/application/presentation/ApplicationForm';
+import { useState } from 'react';
 
 export default function ApplicationDetails() {
-    const { id } = useLocalSearchParams();
+    const item = useLocalSearchParams();
+    const [isEditable, setIsEditable] = useState(false);
+    const { id, fullName, email, comments } = item;
+    const { formData, handleOnChange, handleUpdate } = useApplicationForm({ fullName, email, comments } as any);
     const { showDeleteConfirmation, loading } = useDeleteApplication();
 
     const handleDelete = () => {
@@ -11,16 +17,15 @@ export default function ApplicationDetails() {
             router.dismissAll();
         });
     };
-
     return (
         <View>
             <Text>Application Details View</Text>
-            <TextInput editable={false} placeholder='Full Name' value='Milky Kiwi' />
-            <Button title='Editar' onPress={() => console.log('Editar')} disabled={loading} />
+            <ApplicationForm editable={isEditable} formData={formData} handleOnChange={handleOnChange} handleSubmit={() => handleUpdate(id as string)} />
+            <Button title='Editar' onPress={() => setIsEditable(true)} disabled={isEditable} />
             {loading ? (
                 <ActivityIndicator size="small" />
             ) : (
-                <Button title='Borrar' onPress={handleDelete} disabled={loading} />
+                <Button title='Borrar' onPress={handleDelete} disabled={isEditable} />
             )}
         </View>
     )
--- app/(tabs)/applications/create.tsx
+++ app/(tabs)/applications/create.tsx
@@ -1,41 +1,15 @@
-import { View, TextInput, Button, ActivityIndicator } from 'react-native'
 import { useLocalSearchParams } from 'expo-router';
 import useApplicationForm from '@/modules/application/infraestructure/hooks/useApplicationForm';
+import ApplicationForm from '@/modules/application/presentation/ApplicationForm';
 
 
 export default function CreateApplicationScreen() {
     const { animalId } = useLocalSearchParams();
-    const { loading, formData, handleOnChange, handleSubmit } = useApplicationForm();
-
+    const { formData, handleOnChange, handleSubmit } = useApplicationForm();
     return (
-        <View>
-            <TextInput
-                placeholder='Full Name'
-                value={formData.fullName}
-                onChangeText={
-                    (input: string) => { handleOnChange('fullName', input) }
-                }
-            />
-            <TextInput
-                placeholder='Email'
-                value={formData.email}
-                onChangeText={
-                    (input: string) => { handleOnChange('email', input) }
-                }
-            />
-            <TextInput
-                placeholder='Comments'
-                value={formData.comments}
-                onChangeText={
-                    (input: string) => { handleOnChange('comments', input) }
-                }
-
-            />
-            {loading ? (
-                <ActivityIndicator size="large" />
-            ) : (
-                <Button title='Submit' onPress={() => handleSubmit(animalId as string)} />
-            )}
-        </View>
+        <ApplicationForm
+            formData={formData}
+            handleOnChange={handleOnChange}
+            handleSubmit={() => handleSubmit(animalId as string)} />
     )
 }
