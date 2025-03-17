d8b1965b5b3fb26e818f1959718471f6e825f4c8
Milky Raven <heartonllamas@gmail.com>
2025-03-17 12:34:25 +0100

    enhancement: add more use cases to application module

--- modules/application/domain/repositories/ApplicationRepository.ts
+++ modules/application/domain/repositories/ApplicationRepository.ts
@@ -1,4 +1,7 @@
 import { Application } from "../entities/Application";
 export interface ApplicationRepository {
   findAllApplicationsByUser(userId: string): Promise<Application[]>;
+  createApplication(application: Application): Promise<string>;
+  updateApplication(application: Application): Promise<void>;
+  deleteApplication(id: string): Promise<void>;
 }
