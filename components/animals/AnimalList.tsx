190169259bba4e8b2eba9cb4e2cc78231433cb5e
Milky Raven <heartonllamas@gmail.com>
2025-03-17 19:20:38 +0100

    refactor: implement Get Animals hook in Animal List component

--- components/animals/AnimalList.tsx
+++ components/animals/AnimalList.tsx
@@ -1,51 +1,29 @@
-import React, { useEffect, useState } from "react";
-import { View, FlatList } from 'react-native';
+import React from "react";
+import { View, FlatList, ActivityIndicator } from 'react-native';
 import { Link } from "expo-router";
 import AnimalThumbnail from "./AnimalThumbnail";
-import { animals } from '../../mockData'
-import { useLocation } from "@/modules/location/hooks/useLocation";
-import { CalculateDistanceFromUserToAnimal } from "@/modules/location/application/CalculateDistanceFromUserToAnimal";
-import { CalculateDistanceGeolib } from "@/modules/location/infraestructure/CalculateDistanceGeolib";
-import { Animal, AnimalWithDistance } from "@/modules/animal/domain/entities/Animal";
-import { Location } from "@/modules/location/domain/Location";
-//calculate distance
-const geolib = new CalculateDistanceGeolib();
-const calculateDistance = new CalculateDistanceFromUserToAnimal(geolib);
+import { useGetAnimals } from "@/modules/animal/infraestructure/hooks/useGetAnimals";
 
 export default function AnimalList() {
-    const [animalsWithDistance, setAnimalsWithDistance] = useState<AnimalWithDistance[]>([]);
-    //TO-DO: refactor
+    const { animals, loading } = useGetAnimals();
 
-    const { location } = useLocation();
-
-    function calculateAnimalDistancesFromUser(
-        location: Location,
-        animals: Animal[]
-    ): AnimalWithDistance[] {
-        return animals.map((animal) => ({
-            ...animal,
-            distance: calculateDistance.execute(location, {
-                lat: animal.location.lat,
-                lon: animal.location.lon,
-            }),
-        }));
+    if (loading) {
+        return (
+            <View>
+                <ActivityIndicator size="large" />
+            </View>
+        );
     }
-
-    useEffect(() => {
-        if (location) {
-            const updatedAnimals = calculateAnimalDistancesFromUser(location, animals);
-            setAnimalsWithDistance(updatedAnimals);
-        }
-    }, [location]);
 
     return (
         <View>
             <FlatList
-                data={animalsWithDistance}
+                data={animals}
                 renderItem={({ item }) => (
                     <Link
                         href={{
                             pathname: '/explore/[id]',
+                            // @ts-ignore - Passing complex object as params
                             params: { ...item } //TO-DO: not sure if this is correct, but i want to avoid an extra call
                         }}>
                         <AnimalThumbnail {...item} />
