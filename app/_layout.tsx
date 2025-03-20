685b75c282fd2098086d184665ff015c8e5d41bf
Milky Raven <heartonllamas@gmail.com>
2025-03-20 08:11:34 +0100

    feat: create basic user context to handle user related data 🧝🏼‍♀️

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -2,15 +2,19 @@ import { Slot } from 'expo-router';
 import { ThemeProviderWrapper } from '@/modules/shared/themes/ThemeProviderWrapper';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { StyleSheet } from 'react-native';
+import { UserProvider } from '@/modules/user/infraestructure/UserContext';
 
 export default function App() {
 
   return (
-    <ThemeProviderWrapper>
-      <SafeAreaView style={styles.container}>
-        <Slot />
-      </SafeAreaView>
-    </ThemeProviderWrapper>
+
+    <UserProvider>
+      <ThemeProviderWrapper>
+        <SafeAreaView style={styles.container}>
+          <Slot />
+        </SafeAreaView>
+      </ThemeProviderWrapper>
+    </UserProvider>
   );
 }
 
---
+++ modules/user/domain/entities/User.ts
@@ -0,0 +1,11 @@
+export interface User {
+  id: string;
+  name: string;
+  email: string;
+}
+
+export const defaultUser: User = {
+  id: "5X1x1RbgnCLAoXxBvBOf",
+  name: "Milky",
+  email: "milkykiwidev@gmail.com",
+};
